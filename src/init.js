2e8e974e227a8f4ddeeb2b3313a3fc9121199ee3
Hack Reactor Students <accounts@hackreactor.com>
2014-12-15 23:18:08 -0800

    Implements head link list among many other implementations

--- src/init.js
+++ src/init.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
   window.dancers = [];
 
+  for(var key in heads){
+    var $link = $("<a href='#'></a>");
+    $link.addClass('chooseDancer');
+    $link.data('dancer-maker-function-name', "SpecialDancer");
+    $link.data('name', key);
+    var $profilePic = $('<div></div>');
+    $profilePic.addClass('dancerProfile');
+    $profilePic.css('background-image', 'url("' + heads[key] + '")');
+    var $dancerName = $('<div></div>');
+    $dancerName.addClass('dancerName');
+    $dancerName.text(key)
+    $dancerName.appendTo($profilePic);
+    $profilePic.appendTo($link);
+    $link.appendTo($('.dancerMenu'));
+
+  }
+
   $(".addDancerButton").on("click", function(event){
     /* This function sets up the click handlers for the create-dancer
      * buttons on index.html. You should only need to make one small change to it.
@@ -25,9 +42,36 @@ $(document).ready(function(){
     var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
-      1000
+      500
     );
     $('body').append(dancer.$node);
   });
+
+
+  $('.chooseDancer').on('click', function(){
+    var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
+    var name = $(this).data('name');
+    console.log(name);
+
+    // get the maker function for the kind of dancer we're supposed to make
+    var dancerMakerFunction = window[dancerMakerFunctionName];
+    var dancer = new dancerMakerFunction(
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      500
+    );
+    $('body').append(dancer.$node);
+    // dancer.$node.find('.head').css('background-image', '\name);
+  })
+
 });
+
+var heads = {
+  yellow: "img/heads/smiley.png",
+  red: "img/heads/smiley_red.png",
+  blue: "img/heads/smiley_blue.png"
+};
+
+
+
 
--- src/specialDancer.js
+++ src/specialDancer.js
@@ -1,8 +1,8 @@
 var SpecialDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps);
-  this.$node = $('<span class = "container"><span>');
-  this.$node.head = $('<span class = "container"><span>');
-  this.$node.body = $('<span class = "container"><span>');
+  this.$node = $('<div class = "dancerContainer"><div>');
+  this.$node.append('<div class = "head left"></div>');
+  this.$node.append('<div class = "body"></div>');
   this.setPosition(top, left);
 }
 
@@ -10,5 +10,5 @@ SpecialDancer.prototype = Object.create(Dancer.prototype);
 
 SpecialDancer.prototype.step = function(){
   Dancer.prototype.step.call(this);
-  this.$node.toggleClass("right");
+  this.$node.find('.head').toggleClass("right");
 };
